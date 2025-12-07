3f0af86f235d15c3ac7053f8b8d29ab570576501
Always blue <bluesky.1289@gmail.com>
2025-12-07 15:56:21 +0700

    Initial Commit

---
+++ quickeditTests/quickeditTests.swift
@@ -0,0 +1,17 @@
+//
+//  quickeditTests.swift
+//  quickeditTests
+//
+//  Created by Flex on 12/7/25.
+//
+
+import Testing
+@testable import quickedit
+
+struct quickeditTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
